86a30c87e4fbf63c0ef9559ad729491f79497d41
Farades <creg2007@gmail.com>
2014-12-02 22:42:42 +0300

    some fixed

--- src/gui/FieldGui.java
+++ src/gui/FieldGui.java
@@ -1,29 +1,138 @@
 package gui;
 
+import logic.Cell;
 import logic.Field;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class FieldGui extends JPanel {
     private Field field;
+    private boolean enemyFlag;
+    private FieldCellGui[][] cellsGui;
+    private String[] letters = {"А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "К"};
 
-    public FieldGui(Field _field) {
+    public FieldGui(Field _field, boolean _enemyFlag) {
         field = _field;
-        setLayout(new GridLayout(10, 10, 3, 3));
+        cellsGui = new FieldCellGui[Field.ROW_COUNT][Field.COL_COUNT];
+        enemyFlag = _enemyFlag;
+        setLayout(new GridLayout(Field.ROW_COUNT + 1, Field.COL_COUNT + 1, 3, 3));
         setBackground(Color.white);
-        setBorder(BorderFactory.createLineBorder(new Color(0, 162, 255), 2));
-        reDraw();
+        if (isEnemyField()) {
+            setBorder(BorderFactory.createLineBorder(new Color(255, 9, 36), 2));
+        } else {
+            setBorder(BorderFactory.createLineBorder(new Color(15, 204, 51), 2));
+        }
+        initField();
     }
 
-    private void reDraw() {
+    public boolean isEnemyField() {
+        return enemyFlag;
+    }
+
+    public void invertEnemyFlag() {
+        enemyFlag = !enemyFlag;
+    }
+
+    public void reDrawField() {
+        for (int x = 0; x < Field.ROW_COUNT; x++) {
+            for (int y = 0; y < Field.COL_COUNT; y++) {
+                cellsGui[x][y].reDraw();
+            }
+        }
+    }
+
+    private void addLeters() {
+        for (String letter : letters) {
+            JLabel temp = new JLabel(letter);
+            temp.setHorizontalAlignment(JLabel.CENTER);
+            temp.setVerticalAlignment(JLabel.CENTER);
+            add(temp);
+        }
+    }
+
+    private JLabel labelNumber(int n) {
+        JLabel res = new JLabel("" + n);
+        res.setHorizontalAlignment(JLabel.CENTER);
+        res.setVerticalAlignment(JLabel.CENTER);
+        return res;
+    }
+
+    private void initField() {
+        addLeters();
         for (int x = 0; x < Field.ROW_COUNT; x++) {
             for (int y = 0; y < Field.COL_COUNT; y++) {
                 FieldCellGui temp = new FieldCellGui(field.getCells()[x][y]);
+                cellsGui[x][y] = temp;
                 temp.setBorder(BorderFactory.createLineBorder(Color.gray));
                 int state = temp.getCell().getState();
                 temp.reDraw();
                 add(temp);
+            }
+        }
+    }
+
+    public class FieldCellGui extends JPanel implements MouseListener {
+        private Cell cell;
+
+        public FieldCellGui(Cell _cell) {
+            cell = _cell;
+            addMouseListener(this);
+        }
+
+        public Cell getCell() {
+            return cell;
+        }
+
+        public void mouseClicked(MouseEvent e) {
+            if (isEnemyField()) {
+                cell.fire();
+                reDraw();
+            }
+        }
+
+        public void mouseEntered(MouseEvent e) {
+            if (isEnemyField()) {
+                setBackground(new Color(5, 232, 89));
+            }
+        }
+
+        public void mouseExited(MouseEvent e) {
+            if (isEnemyField()) {
+                reDraw();
+            }
+        }
+
+        public void mousePressed(MouseEvent e) {
+        }
+
+        public void mouseReleased(MouseEvent e) {
+        }
+
+        public void reDraw() {
+            int state = cell.getState();
+            switch (state) {
+                case -1:
+                    setBackground(new Color(235, 235, 235));
+                    break;
+                case 0:
+                    if (isEnemyField()) {
+                        setBackground(new Color(235, 235, 235));
+                    } else {
+                        setBackground(new Color(0, 162, 255));
+                    }
+                    break;
+                case 1:
+                    setBackground(Color.yellow);
+                    break;
+                case 2:
+                    setBackground(new Color(255, 60, 60));
+                    break;
+                default:
+                    ;
+                    break;
             }
         }
     }
--- src/gui/SeaBattleGui.java
+++ src/gui/SeaBattleGui.java
@@ -5,6 +5,8 @@ import logic.Field;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SeaBattleGui extends JFrame {
     private FieldGui userField;
@@ -12,25 +14,52 @@ public class SeaBattleGui extends JFrame {
 
     public final static String TITLE = "JavaBase - SeaBattle";
     public final static int WIDTH = 950;
-    public final static int HEIGHT = 500;
+    public final static int HEIGHT = 550;
 
     public SeaBattleGui(Field _userField, Field _compField) {
-        userField = new FieldGui(_userField);
-        compField = new FieldGui(_compField);
+        userField = new FieldGui(_userField, false);
+        compField = new FieldGui(_compField, true);
         userField.setBounds(10, 10, 450, 450);
         compField.setBounds(480, 10, 450, 450);
+
+        JLabel userLabel = new JLabel("Компьютер сделал ход:");
+        userLabel.setBounds(140, 480, 180, 20);
+        JLabel compLabel = new JLabel("Игрок сделал ход:");
+        compLabel.setBounds(630, 480, 150, 20);
 
         JPanel background = new JPanel();
         background.setLayout(null);
         background.add(userField);
         background.add(compField);
+        background.add(userLabel);
+        background.add(compLabel);
         getContentPane().add(background);
 
         setTitle(TITLE);
+        addMenu();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(300, 300, WIDTH, HEIGHT);
         setVisible(true);
         setResizable(false);
+    }
+
+    private void addMenu() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuDebug = new JMenu("Отладка");
+        JMenuItem menuItemDebugShow = new JMenuItem("Показать скрытое)");
+        menuItemDebugShow.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                compField.invertEnemyFlag();
+                compField.reDrawField();
+            }
+        });
+
+        menuDebug.add(menuItemDebugShow);
+
+        menuBar.add(menuDebug);
+
+        setJMenuBar(menuBar);
     }
 
 }
